bb43abb6f68abf24de5d2358b27ab6453248d2d6
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-01-31 17:59:13 +0100

    ShootingGallery initial functionality
    -Added camera movement
    -Added shooting mechanic
    -Added target mechanic

---
+++ Assets/Scripts/ShootingGallery/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float mouseX;
+    float mouseY;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;    //Si fuese sumando, lo haría al revés.
+        xRotation = Mathf.Clamp(xRotation, -40f, 10f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        yRotation += mouseX;
+        yRotation = Mathf.Clamp(yRotation, -40f, 40f);
+        playerBody.rotation = Quaternion.Euler(0f, yRotation, 0f);
+
+        //playerBody.Rotate(Vector3.up * mouseX); OTRA FORMA
+        
+        
+    }
+}
