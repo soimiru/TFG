ee587f99b0fcdc1835d49d527d69dda917c9d675
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-02-14 13:27:02 +0100

    Timer changed to sprites
    -Implemented method to change sprites showing the time.

--- Assets/Scripts/ShootingGallery/SGGameManager.cs
+++ Assets/Scripts/ShootingGallery/SGGameManager.cs
@@ -10,47 +10,69 @@ public class SGGameManager : MonoBehaviour
     public List<GameObject> patitos;
     public Canvas mainCanvas;
     public Text pointsText;
+    public Text timeText;
 
+    private SGUIManager uiMan;
+    private bool gameOver = false;
     private int points = 0;
+    private int gameSeconds = 30;
     private int randomIndex;
 
     private void Awake()
     {
+        uiMan = mainCanvas.GetComponent<SGUIManager>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitForSpawn());
+        StartGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void StartGame() {
+        //timeText.text = "00:" + gameSeconds;
+        InvokeRepeating("Spawn", 0f, 1.5f);
+        InvokeRepeating("CountDown", 0f, 1f);
     }
 
     public void Spawn() {
-        randomIndex = Random.Range(0, spawnPoints.Count);
-        GameObject patitoNew;
-        if (randomIndex < 3)
-        {  //SPAWNS DERECHA
-            patitoNew = (GameObject)Instantiate(patitos[0], spawnPoints[randomIndex].position, Quaternion.identity);
-            patitoNew.GetComponentInChildren<Target>().myDirection = 0;
+        if (!gameOver) {
+            randomIndex = Random.Range(0, spawnPoints.Count);
+            GameObject patitoNew;
+            if (randomIndex < 3)
+            {  //SPAWNS DERECHA
+                patitoNew = (GameObject)Instantiate(patitos[0], spawnPoints[randomIndex].position, Quaternion.identity);
+                patitoNew.GetComponentInChildren<Target>().myDirection = 0;
+            }
+            else
+            {  //SPAWNS IZQUIERDA
+                patitoNew = (GameObject)Instantiate(patitos[1], spawnPoints[randomIndex].position, Quaternion.identity);
+                patitoNew.GetComponentInChildren<Target>().myDirection = 1;
+            }
         }
-        else 
-        {  //SPAWNS IZQUIERDA
-            patitoNew = (GameObject)Instantiate(patitos[1], spawnPoints[randomIndex].position, Quaternion.identity);
-            patitoNew.GetComponentInChildren<Target>().myDirection = 1;
-        }
-        StartCoroutine(WaitForSpawn());
     }
 
-
-    private IEnumerator WaitForSpawn() {
-        yield return new WaitForSeconds(1);
-
-        Spawn();
+    public void CountDown() {
+        if (gameSeconds == 0)
+        {
+            //timeText.text = "Time's UP!";
+            uiMan.TimeManager(-1);
+            gameOver = true;
+            CancelInvoke("Count");
+        }
+        else
+        {
+            gameSeconds -= 1;
+            uiMan.TimeManager(gameSeconds);
+            //timeText.text = "00:" + gameSeconds;
+        }
+        
     }
+
 
     public void AddPoints(int amount) 
     {
--- Assets/Scripts/ShootingGallery/SGUIManager.cs
+++ Assets/Scripts/ShootingGallery/SGUIManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SGUIManager : MonoBehaviour
 {
 
     private bool paused = false;
+
+    public List<Sprite> timeImages;
+    public List<Image> timePositions;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +34,34 @@ public class SGUIManager : MonoBehaviour
         else {
             Time.timeScale = 0f;
             paused = true;
+        }
+    }
+
+    public void TimeManager(int number) {
+
+        if (number == -1)
+        {
+            //TIMESUP
+            Color colorTransparent = new Color(0f, 0f, 0f, 0f);
+            timePositions[0].color = colorTransparent;
+            timePositions[1].color = colorTransparent;
+            timePositions[3].color = colorTransparent;
+            timePositions[4].color = colorTransparent;
+
+            timePositions[2].sprite = timeImages[10];
+            timePositions[2].rectTransform.sizeDelta = new Vector2(500f, 500f);
+        }
+        else if (number >= 10)
+        {
+            int decenas = number / 10;
+            int unidades = number - (decenas * 10);
+            timePositions[3].sprite = timeImages[decenas];
+            timePositions[4].sprite = timeImages[unidades];
+
+        }
+        else {
+            timePositions[3].sprite = timeImages[0];
+            timePositions[4].sprite = timeImages[number];
         }
     }
 }
