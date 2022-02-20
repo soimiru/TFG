f52b5345ea23b527edf6aa780eb650b867cba80a
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-02-20 17:23:25 +0100

    Improved pieces generation

--- Assets/Scripts/ZooMatch/GridManager.cs
+++ Assets/Scripts/ZooMatch/GridManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     public enum PieceType { 
+        EMPTY,
         NORMAL, 
         COUNT,
     };
@@ -17,6 +18,7 @@ public class GridManager : MonoBehaviour
 
     public int xDim;
     public int yDim;
+    public float fillTime;
 
     public PiecePrefab[] piecePrefabs;
     public GameObject backgroundPrefab;
@@ -39,7 +41,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < yDim; y++)
             {
-                GameObject background = (GameObject)Instantiate(backgroundPrefab, new Vector3(x, y, 0), Quaternion.identity);
+                GameObject background = (GameObject)Instantiate(backgroundPrefab, GetWorldPosition(x, y), Quaternion.identity);
                 //background.transform.parent = transform;
                 background.transform.SetParent(transform, false);
             }
@@ -50,7 +52,7 @@ public class GridManager : MonoBehaviour
         {
             for (int y = 0; y < yDim; y++)
             {
-                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], new Vector3(x, y, 0), Quaternion.identity);
+                /*GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], new Vector3(x, y, 0), Quaternion.identity);
                 //GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], Vector3.zero, Quaternion.identity);
 
                 newPiece.name = "Piece(" + x + "," + y + ")";
@@ -65,9 +67,12 @@ public class GridManager : MonoBehaviour
 
                 if (pieces[x, y].IsColored()) {
                     pieces[x, y].ColorComponent.SetColor((ColorPiece.ColorType)Random.Range(0, pieces[x, y].ColorComponent.NumColors));
-                }
+                }*/
+                SpawnNewPiece(x, y, PieceType.EMPTY);
             }
         }
+
+        StartCoroutine(Fill());
 
     }
 
@@ -77,8 +82,59 @@ public class GridManager : MonoBehaviour
         
     }
 
+    public IEnumerator Fill() {
+
+        while (FillStep()) {
+            yield return new WaitForSeconds(fillTime);
+        }
+    }
+    public bool FillStep() { 
+        bool movedPiece = false;
+        for (int y = yDim-2; y >= 0; y--)
+        {
+            for (int x = 0; x < xDim; x++) {
+                GamePiece piece = pieces[x, y];
+                if (piece.IsMovable()) {
+                    GamePiece pieceBelow = pieces[x, y + 1];
+                    if (pieceBelow.Type == PieceType.EMPTY) {
+                        Destroy(pieceBelow.gameObject);
+                        piece.MovableComponent.Move(x, y + 1, fillTime);
+                        pieces[x, y + 1] = piece;
+                        SpawnNewPiece(x, y, PieceType.EMPTY);
+                        movedPiece = true;
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < xDim; x++) {
+            GamePiece pieceBelow = pieces[x, 0];
+            if (pieceBelow.Type == PieceType.EMPTY) {
+                Destroy(pieceBelow.gameObject); //*********************
+                GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[PieceType.NORMAL], GetWorldPosition(x, -1), Quaternion.identity);
+                newPiece.transform.SetParent(transform, false);
+
+                pieces[x, 0] = newPiece.GetComponent<GamePiece>();
+                pieces[x, 0].Init(x, -1, this, PieceType.NORMAL);
+                pieces[x, 0].MovableComponent.Move(x, 0, fillTime);
+                pieces[x, 0].ColorComponent.SetColor((ColorPiece.ColorType)Random.Range(0, pieces[x, 0].ColorComponent.NumColors));
+                movedPiece = true;
+            }
+        }
+        return movedPiece;
+    }
+
     public Vector2 GetWorldPosition(int x, int y) {
         return new Vector2(transform.position.x - xDim / 2.0f + x,
             transform.position.y + yDim /2.0f - y);
+    }
+
+    public GamePiece SpawnNewPiece(int x, int y, PieceType type) {
+        GameObject newPiece = (GameObject)Instantiate(piecePrefabDict[type], GetWorldPosition(x, y), Quaternion.identity);
+        newPiece.transform.SetParent(transform, false);
+
+        pieces[x, y] = newPiece.GetComponent<GamePiece>();
+        pieces[x, y].Init(x, y, this, type);
+        return pieces[x, y];
     }
 }
--- Assets/Scripts/ZooMatch/MovablePiece.cs
+++ Assets/Scripts/ZooMatch/MovablePiece.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovablePiece : MonoBehaviour
 {
     private GamePiece piece;
+    private IEnumerator moveCoroutine;
 
     private void Awake()
     {
@@ -14,19 +15,40 @@ public class MovablePiece : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Move(int newX, int newY) {
+    public void Move(int newX, int newY, float time) {
+        /*piece.X = newY;
+        piece.Y = newY;
+
+        piece.transform.localPosition = piece.GridRef.GetWorldPosition(newX, newY);*/
+        if (moveCoroutine != null) {
+            StopCoroutine(moveCoroutine);
+        }
+        moveCoroutine = MoveCoroutine(newX, newY, time);
+        StartCoroutine(moveCoroutine);
+    }
+
+    private IEnumerator MoveCoroutine(int newX, int newY, float time) {
+
         piece.X = newY;
         piece.Y = newY;
 
-        piece.transform.localPosition = piece.GridRef.GetWorldPosition(newX, newY);
+        Vector3 startPos = transform.position;
+        Vector3 endPos = piece.GridRef.GetWorldPosition(newX, newY);
+
+        for (float t = 0; t <= 1 * time; t += Time.deltaTime) {
+            piece.transform.position = Vector3.Lerp(startPos, endPos, t / time);
+            yield return 0;
+        }
+        piece.transform.position = endPos;
+
     }
 }
