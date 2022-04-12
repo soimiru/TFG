c4f4fa038f085303c59aa8f678724ddbf249beac
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-04-12 17:24:34 +0200

    Changed Right Movement

--- Assets/EndlessRun/Scripts/PlayerMovement.cs
+++ Assets/EndlessRun/Scripts/PlayerMovement.cs
@@ -7,34 +7,35 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask platformLayerMask;
     private Rigidbody rigidbody;
     private float jumpForce;
-    private Collider collider;
+    private SphereCollider collider;
 
     private void Awake()
     {
         rigidbody = transform.GetComponent<Rigidbody>();
-        collider = transform.GetComponent<Collider>();
+        collider = transform.GetComponent<SphereCollider>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        jumpForce = 5f;
+        jumpForce = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsJumping() ) {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() ) {
             Jump();
         }
-        rigidbody.velocity = new Vector3(1f, rigidbody.velocity.y);
+        //rigidbody.velocity = new Vector3(1f, rigidbody.velocity.y);
+        this.transform.position += Vector3.right * 0.02f;
     }
 
     void Jump(){
         rigidbody.velocity = Vector3.up * jumpForce;
     }
 
-    private bool IsJumping() {
+    private bool IsGrounded() {
         //Returns True when in air - False when grounded
         Debug.Log(Physics.BoxCast(collider.bounds.center, collider.bounds.size, Vector3.down * .1f, Quaternion.identity, 1f, platformLayerMask));
         return Physics.BoxCast(collider.bounds.center, collider.bounds.size, Vector3.down * .1f, Quaternion.identity, 1f, platformLayerMask);
