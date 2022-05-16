88df03823445662be4b7826d2aa9324d1f9c6aaa
Sonia <so.milagro.2018@alumnos.urjc.es>
2022-05-16 13:44:11 +0200

    Main menu UI update
    First version

--- Assets/_General/Scripts/MainMenuUI.cs
+++ Assets/_General/Scripts/MainMenuUI.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField] private Image ZooMatchImage;
+    [SerializeField] private Image EndlessRunnerImage;
+    [SerializeField] private Image ShootingGalleryImage;
+    [SerializeField] private Image SettingsImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +27,25 @@ public class MainMenuUI : MonoBehaviour
     public void ChargeGame(string scene)
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void ChangeImage(int idx) {
+        switch (idx) {
+            case 0:
+                ZooMatchImage.transform.SetSiblingIndex(3);
+                break;
+            case 1:
+                EndlessRunnerImage.transform.SetSiblingIndex(3);
+                break;
+            case 2:
+                ShootingGalleryImage.transform.SetSiblingIndex(3);
+                break;
+            case 3:
+                SettingsImage.transform.SetSiblingIndex(3);
+                break;
+            default:
+                ZooMatchImage.transform.SetSiblingIndex(3);
+                break;
+        }
     }
 }
